fd36e0267c57c3d98f3fc2ad377aff84339bf689
Luis Botello <botello468@gmail.com>
2020-12-11 16:11:48 -0500

    Added interactive graph, with daily case counts

--- CovidDashboard.js
+++ CovidDashboard.js
@@ -1,7 +1,7 @@
 var caseData = new Array();
 var dataObject;
-var dateArray = [];
 var dailyCaseArray = [];
+var dateArray = [];
 
 function myCallback(data) {
   dataObject = data;
@@ -11,28 +11,37 @@ function myCallback(data) {
 window.onload = pageReady;
 
 function pageReady() {
+  //Extract page elements needed to display data
   var covidTotal = document.getElementById('CaseTotal');
   var yesterdayCasesBox = document.getElementById('DailyCases');
-
+  var ctx = document.getElementById('newChart');
+  //Extract data needed, and place into arrays
   for(let i = 0; i < caseData.length; i++) {
+    var recordDate = new Date(Date.parse(caseData[i].Date));
     dailyCaseArray.push(caseData[i].Peel_Public_Health);
-    dateArray.push(caseData[i].date);
+    dateArray.push(recordDate.toDateString());
   }
   //Method to sum array found at https://www.tutorialrepublic.com/faq/how-to-find-the-sum-of-an-array-of-numbers-in-javascript.php
   dailyCases = dailyCaseArray.reduce(function(a,b) {
     return a + b;
   },0);
-
+  //Display info extracted from data on page
   covidTotal.innerHTML = dailyCases;
   yesterdayCasesBox.innerHTML = dailyCaseArray[dailyCaseArray.length-1];
-
-  var svg = d3.select("body").append("svg").attr("height","520").attr("width","100%");
-  svg.selectAll("rect")
-    .data(dailyCaseArray)
-    .enter().append("rect")
-      .attr("height",function(d,i) { return d; })
-      .attr("width","1")
-      .attr("x",function(d,i){ return i*5; })
-      .attr("y",function(d,i){ return 520-(d); });
-
+  //Graph properties
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    options: {
+      // If this isn't set to false it takes up the whole page
+      responsive: false
+    },
+    data: {
+      labels:dateArray,
+      datasets: [{
+        label: "Daily cases",
+        backgroundColor: 'red',
+        data: dailyCaseArray
+      }],
+    }
+  });
 }
